ea30e6897525406fd887d16aa0a944e67feffe2e
Safaa Abdulhadi <s.r.aldulaimy@gmail.com>
2017-06-20 15:36:40 +0300

    Update Download All_Videos

--- demos/Ziggeo_Download_All_videos.cs
+++ demos/Ziggeo_Download_All_videos.cs
@@ -5,32 +5,43 @@ using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
-namespace Delete_all_videos
+
+
+namespace Download_All_Videos
 {
-    public class Delete_All_Videos
-    {
+	public class Download_All_Videos
+	{
         public static void Main(string[] args)
         {
 
             Ziggeo ziggeo = new Ziggeo("APP_TOKEN", "PRIVATE_KEY", "ENCRYPTION_KEY");
 
-            while (true)
+            dynamic AllVideos = JsonConvert.DeserializeObject(ziggeo.videos().index());
+
+            if (AllVideos != null)
             {
-                dynamic AllVideos = JsonConvert.DeserializeObject(ziggeo.videos().index());
-
-                if (AllVideos.count() == 0)
-                    break;
-
                 foreach (var item in AllVideos)
                 {
 
-                    System.Console.WriteLine("Deleting" + item.token);
+                    dynamic videoToDownload = JsonConvert.DeserializeObject(ziggeo.videos().get(item.token));
 
-                    ziggeo.videos().delete(item.token);
+                    string file_extension = videoToDownload.defaultstream.video_type;
+
+                    string file_name = videoToDownload.defaultstream.video_token + "." + file_extension;
+
+
+                    System.Console.WriteLine("downloading" + file_name);
+
+
+                    var file_content = ziggeo.videos().download_video(videoToDownload);
+
+                    string file_path = "your file path" + file_name;
+
+                    File.WriteAllText(file_path, (file_content));
                 }
             }
-
         }
-    }
+
+	}
 }
 
