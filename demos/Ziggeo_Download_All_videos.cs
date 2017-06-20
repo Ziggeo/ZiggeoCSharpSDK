aef485e8e14d6b7cba12c562a54da9864deeaf84
Safaa Abdulhadi <s.r.aldulaimy@gmail.com>
2017-06-20 15:30:43 +0300

    Update Download_ALL_videos.cs
    Update file to allow deletion after 50, and some code enhancement.

--- demos/Ziggeo_Download_All_videos.cs
+++ demos/Ziggeo_Download_All_videos.cs
@@ -1,47 +1,36 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
-
-
-namespace Download_All_Videos
+namespace Delete_all_videos
 {
-	public class Download_All_Videos
-	{
+    public class Delete_All_Videos
+    {
         public static void Main(string[] args)
         {
 
             Ziggeo ziggeo = new Ziggeo("APP_TOKEN", "PRIVATE_KEY", "ENCRYPTION_KEY");
 
-            dynamic AllVideos = JsonConvert.DeserializeObject(ziggeo.videos().index());
-
-            if (AllVideos != null)
+            while (true)
             {
+                dynamic AllVideos = JsonConvert.DeserializeObject(ziggeo.videos().index());
+
+                if (AllVideos.count() == 0)
+                    break;
+
                 foreach (var item in AllVideos)
                 {
 
-                    dynamic videoToDownload = JsonConvert.DeserializeObject(ziggeo.videos().get(item.token));
+                    System.Console.WriteLine("Deleting" + item.token);
 
-                    string file_extension = videoToDownload.defaultstream.video_type;
-
-                    string file_name = videoToDownload.defaultstream.video_token + "." + file_extension;
-
-
-                    System.Console.WriteLine("downloading" + file_name);
-
-
-                    var file_content = ziggeo.videos().download_video(videoToDownload);
-
-                    string file_path = "your file path" + file_name;
-
-                    File.WriteAllText(file_path, (file_content));
+                    ziggeo.videos().delete(item.token);
                 }
             }
-        }
 
-	}
+        }
+    }
 }
 
