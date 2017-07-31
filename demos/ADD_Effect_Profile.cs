243538a6fd9b5f187f1d4a97f0bbd6a9f018cb3b
Safaa Abdulhadi <s.r.aldulaimy@gmail.com>
2017-07-31 13:27:20 +0300

    Create effect profile

---
+++ demos/ADD_Effect_Profile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace Add_effect_Profile
+{
+    public class ADD_Effect_Profile
+    {
+        public static void Main(string[] args)
+        {
+            Ziggeo ziggeo = new Ziggeo("APP_TOKEN", "PRIVATE_KEY", "ENCRYPTION_KEY");
+
+            Dictionary<string, string> profile = new Dictionary<string, string>()
+                {
+                    {"key","YOUR_EFFECT_PROFILE_KEY"},
+                    {"title","YOUR_EFFECT_PROFILE_TITLE" },
+                };
+
+             ziggeo.effectProfiles().create(profile);
+             System.Console.WriteLine("Effect Profile has been created!!");
+
+        }
+    }
+
+}
