f23b1dfba0739f4c0633962e96d707f772d5b4a2
Safaa Abdulhadi <s.r.aldulaimy@gmail.com>
2017-08-03 13:18:47 +0300

    Push to Service

---
+++ demos/Push_To_Service.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace Push_Service
+{
+    public class Push_To_Service
+    {
+		public static void Main(string[] args)
+		{
+			Ziggeo ziggeo = new Ziggeo("APP_TOKEN", "PRIVATE_KEY", "ENCRYPTION_KEY");
+
+			Dictionary<string, string> push = new Dictionary<string, string>()
+				{
+					{"pushservicetoken","YOUR Push Services's token"},
+				};
+
+			ziggeo.videos().update("Video_TOKEN", push);
+			System.Console.WriteLine("Video has now been pushed!!");
+
+		}
+    }
+}
